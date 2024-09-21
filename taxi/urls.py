090d0f685d3a56e724d622699feb1f938431bbb0
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-09-21 11:24:17 +0300

    success

--- taxi/urls.py
+++ taxi/urls.py
@@ -2,8 +2,44 @@ from django.urls import path
 
 from .views import index
 
-urlpatterns = [
-    path("", index, name="index"),
-]
+from .views import (
+    CarDetailView,
+    ManufacturerListView,
+    CarListView,
+    DriverListView,
+    DriverDetailView
+)
 
+urlpatterns = [
+    path(
+        "",
+        index,
+        name="index"
+    ),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail"
+    ),
+    path(
+        "cars/",
+        CarListView.as_view(),
+        name="car-list"
+    ),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver-list"
+    ),
+]
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,36 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    paginate_by = 5
+    queryset = Manufacturer.objects.all().order_by("name")
+    template_name = "templates/taxi/manufacturer_list.html"
+    context_object_name = "manufacturer_list"
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    template_name = "templates/taxi/car_list.html"
+    context_object_name = "car_list"
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+    template_name = "taxi/driver_list.html"
+    context_object_name = "driver_list"
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
